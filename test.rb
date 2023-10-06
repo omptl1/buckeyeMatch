45f1117e717433ff941e744f785afdee65ea2690
pengyu <chen.10921@osu.edu>
2023-10-06 10:52:36 -0400

    organization name with corresponding index and parttially scrpe the content according to the index user choose

--- test.rb
+++ test.rb
@@ -1,4 +1,4 @@
-  require 'mechanize'
+require 'mechanize'
 require 'nokogiri'
 
 # Initialize Mechanize
@@ -21,11 +21,41 @@ page = agent.get(full_url)
 # Parse the fetched page with Nokogiri
 doc = page.parser
 
- organization_names = doc.css('h2.c-card__header').map do |header|
-  header.text.strip
+organization_data = doc.css('h2.c-card__header').map do |header|
+  {
+    name: header.text.strip,
+    id: doc.at_css('body')['id']
+  }
 end
 
-# Print each organization name
-organization_names.each do |name|
-  puts name
-end 
+organization_data.each_with_index do |org, index|
+  puts "#{index + 1}. #{org[:name]}"
+end
+
+# Prompt user to choose a club
+puts "\n Enter the club you're curious about in number:"
+choice = gets.chomp.to_i
+
+# Ensure valid input
+if choice >= 1 && choice <= organization_data.size
+  chosen_org = organization_data[choice - 1]
+  org_id = chosen_org[:id] # Ensure you've extracted the ID correctly above
+
+  details_url = "https://activities.osu.edu/involvement/student_organizations/find_a_student_org?i=#{org_id}&v=card&s=#{search_query}&c=Columbus&page=0"
+  puts details_url
+end
+=begin  
+# Fetch and parse the chosen club's details
+  details_page = agent.get(details_url)
+  club_userwant = Nokogiri::HTML(details_page.body)
+
+  # Target the <th> element that contains the text "Purpose Statement"
+purpose_th = club_userwant.at_css('th:contains("Purpose Statement:") i')
+  # Extract the text content of the following <td> sibling
+  purpose_text = purpose_th.next_element.text.strip
+
+  puts purpose_text
+else
+  puts "Invalid choice!"
+end
+=end
